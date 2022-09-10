dd7eda72370a481df55bb87cae93f1e7c9cdb870
Artem <pointers@bk.ru>
2022-09-10 17:05:34 +0300

    задача решена

--- Zadacha_41/Program.cs
+++ Zadacha_41/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+#nullable disable
+Console.WriteLine("Введите числа через запятую:");
+
+string numbers = Console.ReadLine();
+string[] arrayNumbers = numbers.Split(',');
+
+int[] GetArray(string[] tmp)
+{
+    int len = tmp.Length;
+    int[] newArray = new int[len];
+    int i = 0;
+    for (i = 0; i < len; i++)
+    {
+        newArray[i] = Convert.ToInt32(tmp[i].ToString());
+    }
+    return newArray;
+}
+
+int AboveZero(int[] anyArray)
+{
+    int count = 0;
+    for (int i = 0; i < anyArray.Length; i++)
+    {
+        if (anyArray[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+int[] array = GetArray(arrayNumbers);
+int count = AboveZero(array);
+Console.WriteLine($"Количество чисел больше нуля: {count}");
+
+
